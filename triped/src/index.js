7ca5a5f2a9cfc02c3ae6a1ea4deda52fbb163fc6
Ihor Dubas <ihordubas1525@gmail.com>
2020-02-08 12:07:24 +0200

    Implement router for redirect pages

--- triped/src/index.js
+++ triped/src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {BrowserRouter as Router, Switch, Route, Redirect} from "react-router-dom";
+
 import CardContainer from './components/cardContainer'
+import EditTrip from './components/editTrip'
+
+import './index.css';
+
 
 class Triped extends React.Component {
     render () {
         return (
             <div>
-                <CardContainer />
+                <Router>
+                    <Switch>
+                        <Route exact path='/trips' component={CardContainer}/>
+                        <Route path='/trips/:id' component={EditTrip}/>
+                        <Redirect path='*' to ='trips' />
+                    </Switch>
+                </Router>
             </div>
         )
     }
