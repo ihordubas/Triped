8b9a3efe1a84adc3284bd2e6ef7407482a7c12a7
Ihor Dubas <ihordubas1525@gmail.com>
2020-02-08 12:13:05 +0200

    Create component for editing data in cards with modal window

---
+++ triped/src/components/editTrip.js
@@ -0,0 +1,58 @@
+import React from 'react';
+
+import EditRoundedIcon from '@material-ui/icons/EditRounded';
+import Dialog from "@material-ui/core/Dialog";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Button from '@material-ui/core/Button';
+
+
+export default class EditTrip extends React.Component {
+    state = {
+        open: false
+    };
+   
+    openDialog = () => {
+        this.setState({
+            open: true
+        })
+    };
+
+    closeDialog = () => {
+        this.setState({
+            open: false,
+        })
+    };
+    
+    render() {
+        console.log(this.props)
+        const { open } = this.state
+        const { data } = this.props
+        return (
+            <div>
+                <EditRoundedIcon onClick={this.openDialog}  fontSize="small"/>
+                <Dialog open={open} onClose={this.closeDialog}>
+                    <DialogTitle id="alert-dialog-title">
+                        {data.type}
+                    </DialogTitle>
+                    <DialogContent >
+                        <div className="edit-inputs">
+                            <input placeholder="city"></input>
+                            <input></input>
+                            <input></input>
+                            <input></input>
+                        </div>
+                        <div className="edit-price">
+                            <input></input>
+                        </div>
+                        <div id="buttons-dialog">
+                            <Button onClick={this.closeDialog} >Cancel</Button>
+                            <Button >Save</Button>
+                        </div>
+                    </DialogContent>
+                </Dialog>
+            </div>
+        )
+    }
+    
+}
