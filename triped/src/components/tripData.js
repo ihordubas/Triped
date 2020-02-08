e44ea4303d4556a0885dbdc0d40413d9f11cebb9
Ihor Dubas <ihordubas1525@gmail.com>
2020-02-08 12:13:56 +0200

    Create component for trip informations inside cards

---
+++ triped/src/components/tripData.js
@@ -0,0 +1,77 @@
+import React from 'react';
+
+import CloseRoundedIcon from '@material-ui/icons/CloseRounded';
+import HomeRoundedIcon from '@material-ui/icons/HomeRounded';
+import FlightTakeoffIcon from '@material-ui/icons/FlightTakeoff';
+import DriveEtaRoundedIcon from '@material-ui/icons/DriveEtaRounded';
+import DirectionsBusRoundedIcon from '@material-ui/icons/DirectionsBusRounded';
+
+import EditTrip from './editTrip'
+
+import './cardDetails.css'
+
+
+class HouseData extends React.Component {
+    render () {
+        const { data } = this.props
+        return (
+            <>
+                <div className="card-label">
+                    <div className="card-data">
+                        <div className="label-icon"><HomeRoundedIcon fontSize="large"/></div>
+                        <div className="label-date">
+                            <div>{data.ckeckOut}</div>
+                            <div>{data.checkIn}</div>
+                        </div>
+                        <div className="label-destination">
+                            <div>{data.addressCity}</div>
+                            <div>{data.addressStreet}</div>
+                        </div>
+                        <div className="label-price">{data.price}$</div>
+                    </div>
+                    <div className="card-buttons">
+                        <EditTrip data={data}/>
+                        <CloseRoundedIcon fontSize="small"/>
+                    </div>
+                </div>
+            </>
+         )
+    }
+}
+
+class MovingData extends React.Component {
+    icons = {
+        plane: <FlightTakeoffIcon fontSize="large"/>,
+        car: <DriveEtaRoundedIcon fontSize="large"/>,
+        bus: <DirectionsBusRoundedIcon fontSize="large"/>
+    }
+    
+    render () {
+        const { data } = this.props
+        return (
+            <>  
+                <div className="card-label">
+                    <div className="card-data">
+                        <div className="label-icon">{this.icons[data.type]}</div> 
+                        <div className="label-date">
+                            <div>{data.startDate}</div>
+                            <div>{data.endDate}</div>
+                        </div>
+                        <div className="label-destination">
+                            <div>{data.startPoint}</div>
+                            <div>{data.endPoint}</div>
+                        </div>
+                        <div className="label-price">{data.price}$</div>
+                    </div>
+                    <div className="card-buttons">
+                        <EditTrip data={data}/>
+                        <CloseRoundedIcon fontSize="small"/>
+                    </div>
+                </div>                    
+            </>
+        )
+    }
+}
+
+
+export {HouseData, MovingData}
