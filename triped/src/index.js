8c01530a5f3502fb8861b4a9c636aa13f852b044
Ihor Dubas <ihordubas1525@gmail.com>
2020-02-08 14:53:59 +0200

    Delete unnecessary imports

--- triped/src/index.js
+++ triped/src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router, Switch, Route, Redirect} from "react-router-dom";
 
 import CardContainer from './components/cardContainer'
-import EditTrip from './components/editTrip'
 
 import './index.css';
 
@@ -12,13 +10,7 @@ class Triped extends React.Component {
     render () {
         return (
             <div>
-                <Router>
-                    <Switch>
-                        <Route exact path='/trips' component={CardContainer}/>
-                        <Route path='/trips/:id' component={EditTrip}/>
-                        <Redirect path='*' to ='trips' />
-                    </Switch>
-                </Router>
+                <CardContainer />
             </div>
         )
     }
