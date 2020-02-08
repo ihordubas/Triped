adcae53af0ff3ed57546e08fb1935e0a18e8d70f
Ihor Dubas <ihordubas1525@gmail.com>
2020-02-08 12:15:12 +0200

    Add static data for card

---
+++ triped/src/components/mockedData.js
@@ -0,0 +1,38 @@
+export const data = [
+    {
+        type: 'house', 
+        checkIn: '01.04',
+        ckeckOut: '04.04',
+        price: 105,
+        addressCity: 'Dortmund',
+        addressStreet: 'Shevchenka 3',
+        info: 'Airbnb'
+    },
+    {
+        type: 'bus',
+        price: 40,
+        startPoint: 'Dortmund',
+        endPoint: 'Amsterdam',
+        startDate: '12:10',
+        endDate: '15:00',
+        info: 'Flixbus'
+    },
+    {
+        type: 'plane',
+        price: 10,
+        startPoint: 'Lviv',
+        endPoint: 'Dortmund',
+        startDate: '07:00',
+        endDate: '8:20',
+        info: 'SkyUp'
+    },
+    {
+        type: 'car',
+        price: 50,
+        startPoint: 'Amsterdam',
+        endPoint: 'Rotterdam',
+        startDate: '15:00',
+        endDate: '16:50',
+        info: 'Opel'
+    }
+]
